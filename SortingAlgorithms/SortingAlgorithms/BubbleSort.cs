d381499c814d09a3333ea1af0f7305a66255add5
Sai Akula <saikiranakula@hotmail.com>
2020-03-07 22:09:50 -0600

    sorting algorithms

---
+++ SortingAlgorithms/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    public static class BubbleSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            bool isSorted = true;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+                if (isSorted)
+                    break;
+            }
+            return arr;
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //****1. Bubble Sort ****//
+            // Time complexity : O(n^2)
+            int[] array = { -1, 0, 1, 2, -1, -4 };
+            var sortedArray = BubbleSort.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(sortedArray[i]);
+            }
+            Console.Read();
+        }
+    }
+}
