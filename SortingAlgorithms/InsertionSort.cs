143add2662748ec763a55c94688c6bd117fda6fc
Sai Akula <saikiranakula@hotmail.com>
2020-03-08 17:40:41 -0500

    Insertion sort

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class InsertionSort
+    {
+        public static int[] Sort(int[] arr)
+        {           
+            for(int i = 1; i < arr.Length; i++)
+            {
+               int value = arr[i];
+               int hole = i;
+                while (hole > 0 && arr[i -1] > value)
+                {
+                    arr[hole] = arr[hole - 1];
+                    hole--;
+                }
+                arr[hole] = value;
+            }
+            return arr;
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -9,7 +9,11 @@ namespace SortingAlgorithms
             //****1. Bubble Sort ****//
             // Time complexity : O(n^2)
             int[] array = { -1, 0, 1, 2, -1, -4 };
-            var sortedArray = BubbleSort.Sort(array);
+            // var sortedArray = BubbleSort.Sort(array);
+
+            //****1. Insertion sort ****//
+            // Time complexity : O(n^2)
+            var sortedArray = InsertionSort.Sort(array);
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(sortedArray[i]);
