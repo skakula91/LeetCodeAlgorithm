672a572aaa63f83b7fc47a8b79259d03574c13b4
Sai Akula <saikiranakula@hotmail.com>
2020-03-10 22:49:10 -0500

    binary tree insert and search

---
+++ Datastructures/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    class BinaryTree
+    {
+        public BNode GetNewNode(int data)
+        {
+            BNode newBnode = new BNode(data);
+            newBnode.left = null;
+            newBnode.right = null;
+            return newBnode;
+        }
+        public BNode Insert(BNode root, int data)
+        {
+            if (root == null)
+            {
+                root = GetNewNode(data);              
+            }
+            else if(data <= root.data)
+            {
+                root.left = Insert(root.left, data);
+            }
+            else
+            {
+                root.right = Insert(root.right, data);
+            }
+            return root;
+        }
+
+        public bool Search(BNode root, int data)
+        {
+            if (root == null)
+                return false;
+            else if (root.data == data)
+                return true;
+            else if (root.data >= data) 
+                return Search(root.left, data);
+            else
+                return Search(root.right, data);
+        }
+    }
+
+    public class BNode
+    {
+        public int data;
+        public BNode right;
+        public BNode left;
+
+        public BNode(int x)
+        {
+            data = x;
+        }
+    }
+}
---
+++ Datastructures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Datastructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BNode bnode = null;
+            BinaryTree bTree = new BinaryTree();
+            bnode =  bTree.Insert(bnode, 1);
+            bnode = bTree.Insert(bnode, 10);
+            bnode = bTree.Insert(bnode, 15);
+            bnode = bTree.Insert(bnode, 2);
+            bnode = bTree.Insert(bnode, 45);
+            bnode = bTree.Insert(bnode, 32);
+
+            bool isAvailable = bTree.Search(bnode, 22);
+            if (isAvailable)
+                Console.WriteLine("Matching element found");
+            else
+                Console.WriteLine("Matching element not found");
+
+            Console.ReadKey();
+        }
+    }
+}
