5346be6344b78a46c8dbcf29db284411e6958a7d
Sai Akula <saikiranakula@hotmail.com>
2020-03-12 23:14:23 -0500

    binary tree max, min element, traversals and mirror

--- Datastructures/BinaryTree.cs
+++ Datastructures/BinaryTree.cs
@@ -17,9 +17,9 @@ namespace Datastructures
         {
             if (root == null)
             {
-                root = GetNewNode(data);              
+                root = GetNewNode(data);
             }
-            else if(data <= root.data)
+            else if (data <= root.data)
             {
                 root.left = Insert(root.left, data);
             }
@@ -36,13 +36,93 @@ namespace Datastructures
                 return false;
             else if (root.data == data)
                 return true;
-            else if (root.data >= data) 
+            else if (root.data >= data)
                 return Search(root.left, data);
             else
                 return Search(root.right, data);
         }
-    }
+        // Traversals:
+        // InOrder
+        public void InOrder(BNode root)
+        {
+            if (root == null)
+                return;
+            InOrder(root.left);
+            Console.WriteLine(root.data);
+            InOrder(root.right);
+        }
 
+        // PreOrder
+        public void PreOrder(BNode root)
+        {
+            if (root == null)
+                return;
+            Console.WriteLine(root.data);
+            PreOrder(root.left);
+            PreOrder(root.right);
+        }
+
+        // PostOrder
+        public void PostOrder(BNode root)
+        {
+            if (root == null)
+                return;
+            PostOrder(root.left);
+            PostOrder(root.right);
+            Console.WriteLine(root.data);
+        }
+
+        // Find min element of binaryTree
+        public int MinElement(BNode root)
+        {
+            while (root.left != null)
+            {
+                root = root.left;
+            }
+            return root.data;
+        }
+
+        // Find max element of binaryTree
+        public int MaxElement(BNode root)
+        {
+            while (root.right != null)
+            {
+                root = root.right;
+            }
+            return root.data;
+        }
+
+        // Mirror Binary tree
+        public BNode Mirror(BNode root, BNode mirrorNode)
+        {
+            if (root != null)
+            {
+                mirrorNode = InsertMirror(mirrorNode, root.data);
+                Mirror(root.left, mirrorNode);
+                Mirror(root.right, mirrorNode);
+            }
+            return mirrorNode;
+        }
+
+        public BNode InsertMirror(BNode root, int data)
+        {
+            if (root == null)
+            {
+                root = GetNewNode(data);
+            }
+            else if (data >= root.data)
+            {
+                root.left = InsertMirror(root.left, data);
+            }
+            else
+            {
+                root.right = InsertMirror(root.right, data);
+            }
+            return root;
+
+
+        }
+    }
     public class BNode
     {
         public int data;
--- Datastructures/Program.cs
+++ Datastructures/Program.cs
@@ -8,19 +8,33 @@ namespace Datastructures
         {
             BNode bnode = null;
             BinaryTree bTree = new BinaryTree();
-            bnode =  bTree.Insert(bnode, 1);
-            bnode = bTree.Insert(bnode, 10);
-            bnode = bTree.Insert(bnode, 15);
+            bnode = bTree.Insert(bnode, 4);
             bnode = bTree.Insert(bnode, 2);
-            bnode = bTree.Insert(bnode, 45);
-            bnode = bTree.Insert(bnode, 32);
+            bnode = bTree.Insert(bnode, 1);
+            bnode = bTree.Insert(bnode, 3);
+            bnode = bTree.Insert(bnode, 5);
 
-            bool isAvailable = bTree.Search(bnode, 22);
-            if (isAvailable)
-                Console.WriteLine("Matching element found");
-            else
-                Console.WriteLine("Matching element not found");
+            //bool isAvailable = bTree.Search(bnode, 22);
+            //if (isAvailable)
+            //    Console.WriteLine("Matching element found");
+            //else
+            //    Console.WriteLine("Matching element not found");
 
+            //Traversal
+            //bTree.InOrder(bnode);
+            //bTree.PreOrder(bnode);
+            //bTree.PostOrder(bnode);
+
+            //min element
+            // int value = bTree.MinElement(bnode);
+            // int value = bTree.MaxElement(bnode);
+            //Console.WriteLine(value);
+
+            // Mirror Binary tree
+            bTree.PreOrder(bnode);
+            BNode mirrorNode = null;
+            mirrorNode = bTree.Mirror(bnode,mirrorNode);
+            bTree.PreOrder(mirrorNode);
             Console.ReadKey();
         }
     }
