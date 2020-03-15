87a009e487154f5d431b95826c11392d44e62237
Sai Akula <saikiranakula@hotmail.com>
2020-03-14 20:31:13 -0500

    binary search

---
+++ SortingAlgorithms/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            int start = 0;
+            int end = nums.Length-1;          
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (target == nums[mid])
+                    return mid;
+                if (target < nums[mid])
+                {
+                    end = mid -1;
+                }
+                else
+                {
+                   start = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -5,7 +5,7 @@ namespace SortingAlgorithms
     class Program
     {
         static void Main(string[] args)
-        {        
+        {
             int[] array = { -1, 0, 1, 2, -1, -4 };
             //****1. Bubble Sort ****//
             // Time complexity : O(n^2)
@@ -22,11 +22,16 @@ namespace SortingAlgorithms
             //****4. Quick sort ****//
             // Time complexity best case: O(nlogn)
             // Time complexity best case: O(n^2)
-            QuickSort.Sort(array, 0, array.Length - 1);
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
+            //QuickSort.Sort(array, 0, array.Length - 1);
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            int[] array1 = {-1, 0, 3, 5, 9, 12};
+            int index = BinarySearch.Search(array1, 9);
+            Console.WriteLine(index);
+
             Console.Read();
         }
     }
