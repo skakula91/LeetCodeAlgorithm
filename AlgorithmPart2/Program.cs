9aee37b2e25de1458ed73cf711ba91d89a809baa
Sai Akula <saikiranakula@hotmail.com>
2020-03-23 23:42:39 -0500

    towers of hanoii

---
+++ AlgorithmPart2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //** 1. Towers of Hanoii **//
+            TowersOfHanoii.Towers(3, 'a','b','c');
+            Console.Read();
+        }
+    }
+}
---
+++ AlgorithmPart2/TowersOfHanoii.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPart2
+{
+    public class TowersOfHanoii
+    {
+        public static void Towers(int disks, char src, char aux, char dest)
+        {
+            if(disks == 1)
+            {
+                Console.WriteLine("Disk moved from" + src + "to" + dest);
+                return;
+            }
+            Towers(disks-1, src, dest, aux);
+            Console.WriteLine("Disk moved from" + src + "to" + dest);
+            Towers(disks - 1, aux, src, dest);
+        }
+    }
+}
