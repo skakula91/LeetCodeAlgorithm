9b7563eb02dfa1eb1c96c12c65aaacdaed698b86
Sai Akula <saikiranakula@hotmail.com>
2020-03-25 22:54:28 -0500

    permutations DFS

---
+++ AlgorithmPart2/Permutations.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmPart2
+{
+    public class Permutations
+    {
+        public static  List<IList<int>> result = new List<IList<int>>();
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            Helper(new List<int>(), nums.ToList());
+            return result;
+        }
+        public static void Helper(List<int> slate, List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                result.Add(slate);
+                
+            }
+            else
+            {
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    List<int> temp = new List<int>(nums);
+                    temp.RemoveAt(i);
+
+                    var item = new List<int>(slate);
+                    item.Add(nums[i]);               
+                    Helper(item, temp);
+                }
+            }
+        }
+    }
+}
--- AlgorithmPart2/Program.cs
+++ AlgorithmPart2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace AlgorithmPart2
 {
@@ -7,7 +8,20 @@ namespace AlgorithmPart2
         static void Main(string[] args)
         {
             //** 1. Towers of Hanoii **//
-            TowersOfHanoii.Towers(3, 'a','b','c');
+            //TowersOfHanoii.Towers(3, 'a','b','c');
+
+            //** 2. Permutations **//
+            int[] a = { 1, 2, 3 };
+            var result = Permutations.Permute(a);
+            for(int i = 0; i <result.Count; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int j =0; j< result[i].Count; j++)
+                {
+                    sb.Append(result[i][j].ToString());
+                }
+                Console.WriteLine(sb);
+            }
             Console.Read();
         }
     }
