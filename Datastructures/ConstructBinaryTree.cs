37b5d0e720f715999aa2a259e03d14fb6554876b
Sai Akula <saikiranakula@hotmail.com>
2020-04-05 10:36:07 -0500

    contruct tree from preorder and inorder traversal and unival count

---
+++ Datastructures/ConstructBinaryTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    public class ConstructBinaryTree
+    {
+        Dictionary<int, int> indexDict = new Dictionary<int, int>();
+        public BNode BuildTree(int[] preorder, int[] inorder)
+        {
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                indexDict.Add(inorder[i], i);
+            }
+
+            return Helper(preorder, 0, preorder.Length - 1, inorder, 0, inorder.Length - 1);
+        }
+        public BNode Helper(int[] preorder, int startp, int endp, int[] inorder, int starti, int endi)
+        {
+            if (startp > endp || starti > endi)
+                return null;
+
+            if (startp == endp)
+                return new BNode(preorder[startp]);
+
+            BNode root = new BNode(preorder[startp]);
+
+            var index = indexDict[root.data];
+            var numsleft = index - starti;
+            var numsright = endi - index;
+
+            root.left = Helper(preorder, startp + 1, startp + numsleft, inorder, starti, index - 1);
+            root.right = Helper(preorder, startp + numsleft+1, endp, inorder, index + 1, endi);
+
+            return root;
+        }
+    }
+}
---
+++ Datastructures/IsUnivalBTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    public class IsUnivalBTree
+    {
+        public int count = 0;
+        public int CountUnivalSubtrees(BNode root)
+        {
+            if (root == null)
+                return 0;
+
+            IsUnival(root);
+            return count;
+        }
+        private bool IsUnival(BNode node)
+        {
+            //Base
+            if (node.left == null && node.right == null)
+            {
+                count = count + 1;
+                return true;
+            }
+
+            bool isUnival = true;
+            if (node.left != null)
+            {
+                bool bl = IsUnival(node.left);
+                if (bl == false || node.data != node.left.data)
+                {
+                    isUnival = false;
+                }
+            }
+            if (node.right != null)
+            {
+                bool br = IsUnival(node.right);
+                if (br == false || node.data != node.right.data)
+                {
+                    isUnival = false;
+                }
+            }
+
+            if (isUnival)
+                count = count + 1;
+
+            return isUnival;
+
+        }
+    }
+}
