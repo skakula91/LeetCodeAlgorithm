aa5cc71c353be023483707d4b207718469323b06
Sai Akula <Sai.Akula@csgi.com>
2020-04-10 15:46:42 -0500

    Number of Connected Components in an Undirected Graph

---
+++ Graphs/NumberofConnectedCompGraph.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs
+{
+    public class NumberofConnectedCompGraph
+    {
+        public int CountComponents(int n, int[][] edges)
+        {
+            var adjacencyMap = BuildGraph(n, edges);
+            int[] visited = new int[n];
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (visited[i] == 0)
+                {
+                    BFS(i, adjacencyMap, visited);
+                    ++count;
+                }
+
+            }
+            return count;
+        }
+
+        public void BFS(int src, Dictionary<int, List<int>> adjacencyMap, int[] visited)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(src);
+            while (q.Count != 0)
+            {
+                var temp = q.Dequeue();
+                visited[temp] = 1;
+                foreach (int edge in adjacencyMap[temp])
+                {
+                    if (visited[edge] == 0)
+                    {
+                        q.Enqueue(edge);
+                        visited[edge] = 1;
+                    }
+                }
+            }
+            return;
+        }
+
+        public Dictionary<int,List<int>> BuildGraph(int n, int[][] edges)
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                dict.Add(i, new List<int>());
+            }
+            // Adjacency List
+            foreach(var edge in edges)
+            {
+               dict[edge[0]].Add(edge[1]);
+               dict[edge[1]].Add(edge[0]);
+            }
+
+            return dict;
+        }
+    }
+}
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -6,7 +6,11 @@ namespace Graphs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1. Number of Connected Components in an Undirected Graph
+            int[][] edges =  {new int[] { 0, 1 }, new int[] { 1, 2 }, new int[] { 3, 4 } };
+            int numberOfComponents = new NumberofConnectedCompGraph().CountComponents(5, edges);
+            Console.WriteLine(numberOfComponents);
+            Console.Read();
         }
     }
 }
