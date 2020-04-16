d8b786ee4316fef25653e61edd16b6f78c1039be
Sai Akula <saikiranakula@hotmail.com>
2020-04-15 21:16:08 -0500

    dynamic programming

---
+++ DynamicProgramming/Climbingstairs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    public class Climbingstairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1 || n == 2)
+                return n;
+            int[] uniqueWays = new int[n];
+            uniqueWays[0] = 1;
+            uniqueWays[1] = 2;
+           
+            for(int i = 2; i < n; i++)
+            {
+                uniqueWays[i] = uniqueWays[i - 1] + uniqueWays[i - 2];
+            }
+            return uniqueWays[n-1];
+        }
+    }
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Climbing Stairs
+            var uniqueWay = new Climbingstairs().ClimbStairs(3);
+            Console.WriteLine(uniqueWay);
+            Console.Read();
+        }
+    }
+}
