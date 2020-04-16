18986f048a8aa8b71a74b55c0e06711932af808a
Sai Akula <saikiranakula@hotmail.com>
2020-04-15 23:42:33 -0500

    unique paths and counting subsets

---
+++ DynamicProgramming/CountSubsetKsize.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    public class CountSubsetKsize
+    {
+        // node = node on the row above + node on the row beside above
+        //c(n,k) = c(n-1,k) + c(n-1,k-1)
+        public int Count(int n, int k)
+        {
+            if (k == 0 || k == n)
+                return 1;
+
+            int[,] memo = new int[n+1,k+1];
+            for(int row =0; row <= n; row++)
+            {
+                memo[row,0] = 1;
+            }
+            for (int col = 0; col <= k; col++)
+            {
+                memo[col, col] = 1;
+            }
+
+            for(int i = 2; i <= n; i++)
+            {
+                // n is > k.. k greater than n doesn't make sence for nck computation
+                for(int j = 1; j<= Math.Min(i,k); j++)
+                {
+                    memo[i, j] = memo[i - 1, j] + memo[i - 1, j - 1];
+                }
+
+            }
+            return memo[n, k];
+        }
+    }
+}
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -7,8 +7,16 @@ namespace DynamicProgramming
         static void Main(string[] args)
         {
             // 1. Climbing Stairs
-            var uniqueWay = new Climbingstairs().ClimbStairs(3);
-            Console.WriteLine(uniqueWay);
+            //var uniqueWay = new Climbingstairs().ClimbStairs(3);
+            //Console.WriteLine(uniqueWay);
+
+            // 2. Counting subset of size k
+            //var count = new CountSubsetKsize().Count(5,2);
+            //Console.WriteLine(count);
+
+            // 3. Counting subset of size k
+            var count = new UniquePaths().UniquePathsCount(2, 2);
+            Console.WriteLine(count);
             Console.Read();
         }
     }
