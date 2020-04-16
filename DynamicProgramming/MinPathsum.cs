9cac901e3e54067a61f253b94750a73f046b3f70
Sai Akula <Sai.Akula@csgi.com>
2020-04-16 14:19:22 -0500

    min path sum

---
+++ DynamicProgramming/MinPathsum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    public class MinPathsum
+    {
+        public int Pathsum(int[][] grid)
+        {
+            int n = grid.Length; // row
+            int m = grid[0].Length; //col
+
+            var memo = new int[n, m];
+
+            memo[0,0] = grid[0][0];
+            for (int i = 1; i < m; i++)
+            {
+                memo[0, i] = memo[0, i - 1] + grid[0][i];
+            }
+
+            for (int j = 1; j < n; j++)
+            {
+                memo[j, 0] = memo[j - 1, 0] + grid[j][0];
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < m; j++)
+                {
+                    memo[i, j] = grid[i][j] + Math.Min(memo[i - 1, j], memo[i, j - 1]);
+                }
+            }
+
+            return memo[n-1, m-1];
+        }
+    }
+}
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -14,9 +14,14 @@ namespace DynamicProgramming
             //var count = new CountSubsetKsize().Count(5,2);
             //Console.WriteLine(count);
 
-            // 3. Counting subset of size k
-            var count = new UniquePaths().UniquePathsCount(2, 2);
-            Console.WriteLine(count);
+            // 3. Unique paths
+            //var count = new UniquePaths().UniquePathsCount(2, 2);
+            //Console.WriteLine(count);
+
+            // 4. Min path sum
+            int[][] input = {new int[] {1, 2, 5}, new int[] {3, 2, 1}};
+            var pathSum = new MinPathsum().Pathsum(input);
+            Console.WriteLine(pathSum);
             Console.Read();
         }
     }
