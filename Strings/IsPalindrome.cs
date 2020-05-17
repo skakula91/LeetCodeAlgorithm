7e1e31979671f6587cf85f5c64d49421aa133ab9
Sai Akula <saikiranakula@hotmail.com>
2020-05-17 12:09:45 -0500

    strings algorithms

---
+++ Strings/IsPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strings
+{
+    public class IsPalindrome
+    {
+        public bool IsPalindromeStr(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while(left < right)
+            {
+                if(!char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+                else if(!char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+                else
+                {
+                    if(char.ToLower(s[left]) != char.ToLower(s[right]))
+                    {
+                        return false;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //****1. Is string a palindrome ****//
+            string s = "A man, a plan, a canal: Panama";
+            bool isPalindrome = new IsPalindrome().IsPalindromeStr(s);
+            Console.WriteLine(isPalindrome);
+            Console.ReadKey();
+        }
+    }
+}
