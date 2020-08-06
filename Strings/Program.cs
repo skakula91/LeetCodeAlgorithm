4f9c4307a4df05d6748f62760fe3a1d41f9c97f2
Sai Akula <Sai.Akula@csgi.com>
2020-08-05 23:04:25 -0500

    tries

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -12,10 +12,20 @@ namespace Strings
             //Console.WriteLine(isPalindrome);
 
             //****2. Substring KMP ****//
-            string s = "aaaab";
-            string pattern = "aab";
-            bool isSubstring = new SubstringMatch_KMP().SubstringIndex(s, pattern);
-            Console.WriteLine(isSubstring);
+            //string s = "aaaab";
+            //string pattern = "aab";
+            //bool isSubstring = new SubstringMatch_KMP().SubstringIndex(s, pattern);
+            //Console.WriteLine(isSubstring);
+
+            //****3. Trie ****//
+            Trie trie = new Trie();
+            trie.Insert("apple");
+            trie.Insert("bat");
+            Console.WriteLine(trie.Search("apple"));
+            Console.WriteLine(trie.Search("app"));
+            Console.WriteLine(trie.StartsWith("app")); // returns true
+            trie.Insert("app");
+            Console.WriteLine(trie.Search("app"));
             Console.ReadKey();
         }
     }
